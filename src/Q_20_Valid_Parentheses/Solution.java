f07e314bf916cd83e620d0d536a761ee8be9c6f6
kanishknishar <kanishknishar@outlook.com>
2026-02-03 12:13:19 +0530

    Add initial project setup with .gitignore and XML configuration files

---
+++ src/Q_20_Valid_Parentheses/Solution.java
@@ -0,0 +1,4 @@
+package Q_20_Valid_Parentheses;
+
+public class Solution {
+}
---
+++ src/Q_21_Merge_Two_Sorted_Linked_List/Solution.java
@@ -0,0 +1,74 @@
+package Q_21_Merge_Two_Sorted_Linked_List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return iterative(list1, list2);
+    }
+
+    private ListNode recursive(ListNode list1, ListNode list2) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    private ListNode iterative(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode tracker1 = list1;
+        ListNode tracker2 = list2;
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+
+        while (tracker1 != null && tracker2 != null) {
+            int val1 = tracker1.val;
+            int val2 = tracker2.val;
+
+            if (val1 < val2) {
+                tail.next = tracker1;
+                ListNode next = tracker1.next;
+                tracker1.next = null;
+                tracker1 = next;
+            } else {
+                tail.next = tracker2;
+                ListNode next = tracker2.next;
+                tracker2.next = null;
+                tracker2 = next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (tracker1 == null) tail.next = tracker2;
+        else if (tracker2 == null) tail.next = tracker1;
+
+        return dummyHead.next;
+    }
+
+    public class ListNode {
+        ListNode next;
+        int val;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
